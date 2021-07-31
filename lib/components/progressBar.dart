eaa55074006a7a23e8a3be3e0cc8e051ea8a2389
unknown <arjun.eversoft@gmail.com>
2021-07-31 21:12:56 +0545

    internet download and upload speed test

---
+++ lib/components/progressBar.dart
@@ -0,0 +1,23 @@
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:speed_test/constants/palette.dart';
+
+class ProgressBar {
+  LinearPercentIndicator showBar(double displayPer) {
+    return new LinearPercentIndicator(
+      width: 300,
+      lineHeight: 24.0,
+      percent: displayPer / 100.0,
+      center: Text(
+        displayPer.toStringAsFixed(1) + "%",
+        style: new TextStyle(
+          fontSize: 14.0,
+          color: txtCol,
+        ),
+      ),
+      linearStrokeCap: LinearStrokeCap.roundAll,
+      backgroundColor: progressBg,
+      progressColor: progressFill,
+    );
+  }
+}
---
+++ lib/constants/testServer.dart
@@ -0,0 +1,2 @@
+String downloadServer = 'http://speedtest.tele2.net/1MB.zip';
+String uploadServer = 'http://speedtest.tele2.net/upload.php';
